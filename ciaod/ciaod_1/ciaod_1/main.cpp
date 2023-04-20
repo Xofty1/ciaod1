81d6eb42fdb291dead835cb534d856439be51af6
Artem <blekart04@mail.ru>
2023-04-21 00:16:56 +0300

    without vector

--- ciaod/ciaod_1/ciaod_1/main.cpp
+++ ciaod/ciaod_1/ciaod_1/main.cpp
@@ -10,27 +10,60 @@ int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	const int MAX_PRODUCTS = 100;
 	setlocale(LC_ALL, "RUS");
-	int operation;
-	int numberOfProducts = 0;
-	product products[MAX_PRODUCTS];
-	cout << "1. ќпределить стоимость покупки по заказу" << '\n' << "2. ƒобавить в список заказа новый товар" << '\n' << "3. ƒобавить новый заказ в список заказов супермаркета" << '\n' << "4. ”далить заказ с заданным номером из списка заказов супермаркета" << endl;
-	cin >> operation;
 
-	if (operation == 1)
+	const int MAX_PRODUCTS =256;
+	const int MAX_ORDER = 256;
+	int operation=0;
+	int numberOfProducts = 0;
+	int numberOfOrder = 0;
+	product products[MAX_PRODUCTS];
+	order orders[MAX_ORDER];
+
+	cout << "1. ќпределить стоимость покупки по заказу" << '\n' << "2. ƒобавить в список заказа новый товар" << '\n' << "3. ƒобавить новый заказ в список заказов супермаркета" << '\n' << "4. ”далить заказ с заданным номером из списка заказов супермаркета\n" << "5. ¬ывести список продуктов заказа по номеру"<< endl;
+
+	while (operation != 6)
 	{
-		product new_product{};
-		new_product = input_product(new_product);
-		numberOfProducts = addProduct
-		(products, new_product, numberOfProducts);
-		print(products, numberOfProducts);
+		cin >> operation;
+		if (operation == 1)
+		{
+			int order_num;
+			cout << "¬ведите номер заказа, цену которого вы хотите узнать: ";
+			cin >> order_num;
+			cost_of_order(orders, order_num, numberOfOrder);
+		}
+		if (operation == 2)
+		{
+
+			int order_num;
+			cout << "¬ведите номер заказа, в который вы хотите добавить товар: ";
+			cin >> order_num;
+			addProdInOrder(order_num, orders, numberOfOrder);
+		}
+		if (operation == 3)
+		{
+			order new_order{};
+			new_order = input_order(new_order, products);
+			numberOfOrder = addOrder(orders, new_order, numberOfOrder);
+			printOrder(orders, numberOfOrder);
+		}
+		if (operation == 4)
+		{
+			int order_num;
+			cout << "¬ведите номер заказа, который вы хотите убрать: ";
+			cin >> order_num;
+			numberOfOrder = DeleteOrder(orders, order_num, numberOfOrder);
+			printOrder(orders, numberOfOrder);
+		}
+		if (operation == 5) //узнать продукты в заказе
+		{
+			int order_num;
+			cout << "¬ведите номер заказа, в котором вы хотите посмотреть продукты: ";
+			cin >> order_num;
+			printProduct(numberOfOrder, orders, order_num);
+		}
+
 	}
 	
-
-	
-
-
-
 	return 0;
 }
--- ciaod/ciaod_1/ciaod_1/main.h
+++ ciaod/ciaod_1/ciaod_1/main.h
@@ -19,18 +19,21 @@ struct order // заказ
 	int day;//день
 	int month;//месяц
 	int year;//год
-	string Surname;//фамилия заказчика
+	char Surname[100];//фамилия заказчика
 	int phone_number;//номер телефона
-	string products_list;//список товаров
+	product *products_list = new product[256];//список товаров
+	int quantity_of_products=0;
+	int sum = 0;
+	
 };
 
-int addProduct(product products[100], product p, int numberOfProducts)
+int addProduct(product products[256], product p, int numberOfProducts)
 	{
 	products[numberOfProducts] = p;
 	numberOfProducts += 1;
 	return numberOfProducts;
-
 	}
+
 product input_product(product p1)
 {
 	cout << "Введите название продукта: ";
@@ -47,15 +50,117 @@ product input_product(product p1)
 	return p1;
 }
 
-void print(product p[100], int d)
+order input_order(order p1, product p[256])
+{
+	cout << "Введите номер заказа: ";
+	cin >> p1.order_number;
+
+	cout << "Введите день: ";
+	cin >> p1.day;
+
+	cout << "Введите месяц: ";
+	cin >> p1.month;
+
+	cout << "Введите год: ";
+	cin >> p1.year;
+
+	cout << "Введите фамилию заказчика: ";
+	cin >> p1.Surname;
+
+	cout << "Введите номер телефона: ";
+	cin >> p1.phone_number;
+	
+	return p1;
+}
+
+void printProduct(int d, order orders[256], int num_ord)
+{
+	int flag = 0;
+	for (int g = 0; g < d; g++)
+	{
+		if (num_ord == orders[g].order_number)
+		{
+			flag = 1;
+			for (int i = 0; i < orders[g].quantity_of_products; i++)
+			{
+				cout << i + 1 << ')' << '\t' << (orders+g)->products_list[i].name << '\t' << orders[g].products_list[i].code << '\t' << orders[g].products_list[i].quantity << '\t' << orders[g].products_list[i].cost << endl;
+			}
+			break;
+		}
+	}
+	if (flag == 0)
+	{
+		cout << "Такого заказа не существует" << endl;
+	}
+}
+
+int addOrder(order orders[256], order p, int numberOfOrder)
+{
+	orders[numberOfOrder] = p;
+	numberOfOrder += 1;
+	return numberOfOrder;
+}
+
+void printOrder(order p[256], int d)
 {
 	for (int i = 0; i < d; i++)
 	{
-		cout << i + 1 <<')' << '\t' << p[i].name << '\t' << p[i].code << '\t' << p[i].quantity << '\t' << p[i].cost << endl;
+		cout << i + 1 << ")  " << "Номер заказа : " << p[i].order_number << '\t' << "День : " << p[i].day << '\t' << "Месяц : " << p[i].month << '\t' << "Год : "  << p[i].year << '\t' << "Фамилия заказчика : " << p[i].Surname << '\t' << "Номер телефона : "  << p[i].phone_number << '\t' <<"Стоимость : "  << p[i].sum << endl;
 	}
-	
 }
 
-//void addOrder()
+void addProdInOrder(int ord, order orders[256], int d) // d - сколько продуктов в заказе, сама функция добавлет в заказ по номеру новый продукт
+{
+	int flag = 0;
+	for (int i = 0; i<d; i++)
+	{
+		if (ord == orders[i].order_number)
+		{
+			product new_product{};
+			new_product = input_product(new_product);
+			orders[i].products_list[orders[i].quantity_of_products] = new_product;
+			orders[i].quantity_of_products += 1;
+			flag = 1;
+			orders[i].sum += new_product.cost;
+			break;
+		}
+	}
+	if (flag == 0)
+	{
+		cout << "Такого заказа не существует\n";
+	}
+}
 
+int DeleteOrder(order orders[256], int num_ord, int d)
+{
+	int flag = 0;
+	for (int i = 0; i < d; i++)
+	{
+		if (num_ord == orders[i].order_number)
+		{
+			for (int j = i; j < d-1; j++)
+			{
+				orders[j] = orders[j + 1];
+			}
+			flag = 1;
+			return d - 1;
+		}
+	}
+	if (flag == 0)
+	{
+		cout << "Такого заказа не существует\n";
+		return d;
+	}
+}
+
+void cost_of_order(order orders[256], int num_ord, int d)
+{
+	for (int i = 0; i < d; i++)
+	{
+		if (num_ord == orders[i].order_number)
+		{
+			cout << "Цена данного заказа: " << orders[i].sum << endl;
+		}
+	}
+}
 #endif
